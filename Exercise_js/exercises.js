c2a5e0e11831fbe23be2965f8595f0573444edba
Dani <drifdani@hotmail.com>
2016-09-21 18:30:05 +0200

    uptdated js exercises

--- Exercise_js/exercises.js
+++ Exercise_js/exercises.js
@@ -104,7 +104,6 @@ function conversion(){
 	do{
 		var moneda = prompt("inserte el tipo de moneda","EUR o USD");
 		if(moneda == null) return;
-
 	}while(moneda != "EUR" && moneda != "USD");
 
 	do{
@@ -182,8 +181,47 @@ function fecha(){
 	}while(year < 1950 || year > 2013);
 	
 	var weekday = ["Domingo","Lunes","Martes","Miercoles","Jueves","Viernes","Sabado"];
-	var months = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembte","Diciembre"];
+	var monthName;
 
-	document.getElementById("canvas").innerHTML = "<div class=\"well\">" + weekday[new Date().getDay()] + ", " + day + " de " + months[month-1] + " de " + year + "</div>";
-
+	switch(month){
+		case 1:
+			monthName = "Enero";
+			break;
+		case 2:
+			monthName = "Febrero";
+			break;
+		case 3:
+			monthName = "Marzo";
+			break;
+		case 4:
+			monthName = "Abril";
+			break;
+		case 5:
+			monthName = "Mayo";
+			break;
+		case 6:
+			monthName = "Junio";
+			break;
+		case 7:
+			monthName = "Julio";
+			break;
+		case 8:
+			monthName = "Agosto";
+			break;
+		case 9:
+			monthName = "Septiembre";
+			break;
+		case 10:
+			monthName = "Octubre";
+			break;
+		case 11:
+			monthName = "Noviembte";
+			break;
+		case 12:
+			monthName = "Diciembre";
+			break;
+	}
+	var date = new Date(year + "/" + month + "/" + day);
+	var cadena = "<div class=\"well\">" + weekday[date.getDay()] + ", " + day + " de " + monthName + " de " + year + "</div>";
+	document.getElementById("canvas").innerHTML = cadena;
 }
